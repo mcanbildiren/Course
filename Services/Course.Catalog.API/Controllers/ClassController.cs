2e1323535eafeb60aa33f513e03e380ea2786192
Mustafa Can Bildiren <78155451+mcanbildiren@users.noreply.github.com>
2024-09-12 00:49:43 +0300

    Course controller added.

---
+++ Services/Course.Catalog.API/Controllers/ClassController.cs
@@ -0,0 +1,11 @@
+﻿using Course.Catalog.API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Catalog.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClassController(IClassService classService) : ControllerBase
+{
+    
+}
